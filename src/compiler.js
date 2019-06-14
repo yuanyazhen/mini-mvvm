d3569e7de038a04df0743de802fe20285cf382ec
yuanyazhen <yazhener@icloud.com>
2019-06-14 08:34:10 +0800

    feat: start complie

--- src/compiler.js
+++ src/compiler.js
@@ -1,29 +1,41 @@
-class Compiler{
+export default class Compiler{
     constructor(el, vm) {
-        // 在文档片段中解析，解析完成之后再填入真实文档
         this.el = el;
         this.vm = vm;
 
+        // 1.真实 DOM 移入到内存 fragment 中
         const fragment = this.dom2fragment(this.el);
+        // 2.提取指令 v-model 等 和 {{text}}
         this.compile(fragment);
+        // 3.将编译好的 fragment 赛回到真实的DOM
+        this.el.appendChild(fragment);
     }
+    /* utils func */
     isElementNode(node) {
         return node.nodeType === 1;
     }
+    /* core function */
     dom2fragment(el) {
         const fragment = document.createDocumentFragment();
         [...el.childNodes].forEach(item => fragment.appendChild(item));
         return fragment;
     }
-    compileElement(node) {}
+    compileElement(node) {
+
+    }
     compileText(node) {
         // {{a}} {{b}}
-        
+        // const a = 
+        console.log('text =>', node.textContent)
+        const expr = node.textContent
+        if(expr) {
+            compileUtils.text(node, expr, this.vm)
+        }
     }
     compile(fragment) {
         let childNodes = fragment.childNodes;
         [...childNodes].forEach(node => {
-            if(isElementNode(node)) {
+            if(this.isElementNode(node)) {
                 this.compileElement(node)
                 this.compile(node)
             } else {
@@ -35,4 +47,19 @@ class Compiler{
     }
 }
 
-export default Compiler
+const compileUtils = {
+    /**
+     * 
+     * @param {当前 DOM 节点} node 
+     * @param {提取出的表达式} expr 
+     * @param {需要在 vm 里面拿到 data} vm 
+     */
+    text(node, expr, vm) {
+        const param = expr.match(/\{\{(.*?)\}\}/g);
+    },
+    updater: {
+        textUpdater() {
+            node.replace(expr, )
+        }
+    }
+}
